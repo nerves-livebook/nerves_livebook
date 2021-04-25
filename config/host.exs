247a206341435ca4345242216169af76700cb858
Frank Hunleth <fhunleth@troodon-software.com>
2021-04-25 16:40:46 -0400

    mix format

--- config/host.exs
+++ config/host.exs
@@ -9,7 +9,7 @@ config :livebook, LivebookWeb.Endpoint,
   secret_key_base: "9hHHeOiAA8wrivUfuS//jQMurHxoMYUtF788BQMx2KO7mYUE8rVrGGG09djBNQq7"
 
 config :livebook,
-root_path: "priv/livebooks"
+  root_path: "priv/livebooks"
 
 config :nerves_runtime,
   target: "host"
