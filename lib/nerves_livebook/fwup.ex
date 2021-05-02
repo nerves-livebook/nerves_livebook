e633fe36b195e9df2161fa587b51dcd1780c5aa4
Frank Hunleth <fhunleth@troodon-software.com>
2021-05-02 09:38:50 -0400

    Validate firmware signature and clean up

--- lib/nerves_livebook/fwup.ex
+++ lib/nerves_livebook/fwup.ex
@@ -2,28 +2,34 @@ defmodule NervesLivebook.Fwup do
   @doc """
   Upgrades the system with the specified firmware update file
   """
-  @spec upgrade(String.t()) :: :ok | {:error, :error_exit}
-  def upgrade(path) do
+  @spec upgrade(String.t(), [String.t()]) :: :ok | {:error, :error_exit}
+  def upgrade(path, extra_arguments \\ []) do
     devpath = Nerves.Runtime.KV.get("nerves_fw_devpath")
 
-    args = [
-      "--apply",
-      "--no-unmount",
-      "-d",
-      devpath,
-      "--task",
-      "upgrade",
-      "-i",
-      path
-    ]
+    args =
+      [
+        "--apply",
+        "--no-unmount",
+        "-d",
+        devpath,
+        "--task",
+        "upgrade",
+        "-i",
+        path
+      ] ++ extra_arguments
 
     case System.cmd("fwup", args, into: IO.stream(:stdio, :line)) do
       {_, 0} ->
-        IO.puts("Going to reboot in 5 seconds!!!")
+        IO.write("Going to reboot in 5")
+
+        for i <- 4..1 do
+          Process.sleep(1000)
+          IO.write(", #{i}")
+        end
 
         _ =
           spawn(fn ->
-            Process.sleep(5000)
+            Process.sleep(1000)
             Nerves.Runtime.reboot()
           end)
 
