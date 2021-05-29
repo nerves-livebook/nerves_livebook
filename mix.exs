df0a4e142fab9768cc2fe56c7477ddac8166e1a8
Alex McLain <alex@alexmclain.com>
2021-05-29 15:49:55 -0700

    Limit pigpiox dependency to Raspberry Pi targets.

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule NervesLivebook.MixProject do
       {:circuits_gpio, "~> 0.4", targets: @all_targets},
       {:circuits_i2c, "~> 0.3", targets: @all_targets},
       {:circuits_spi, "~> 0.1", targets: @all_targets},
-      {:pigpiox, "~>0.1"},
+      {:pigpiox, "~>0.1", targets: [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4]},
       {:power_control, github: "cjfreeze/power_control", targets: @all_targets},
       {:ramoops_logger, "~> 0.1", targets: @all_targets},
       {:bmp280, "~> 0.2", targets: @all_targets},
