199f3b6cdc2e9115bbcc5d66f6387b207771b9e8
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-09-08 07:56:54 -0400

    Print MOTD in iex.exs

--- rootfs_overlay/etc/iex.exs
+++ rootfs_overlay/etc/iex.exs
@@ -1,24 +1,4 @@
-IO.puts("""
-\e[34m████▄▖    \e[36m▐███
-\e[34m█▌  ▀▜█▙▄▖  \e[36m▐█
-\e[34m█▌ \e[36m▐█▄▖\e[34m▝▀█▌ \e[36m▐█   \e[39mN  E  R  V  E  S
-\e[34m█▌   \e[36m▝▀█▙▄▖ ▐█
-\e[34m███▌    \e[36m▀▜████\e[0m
-""")
+NervesMOTD.print()
 
 # Add Toolshed helpers to the IEx session
 use Toolshed
-
-if RingLogger in Application.get_env(:logger, :backends, []) do
-  IO.puts("""
-
-  All of the Nerves projects are available in this firmware
-  image. See https://github.com/fhunleth/nerves_livebook for
-  more details.
-
-  View log messages with `RingLogger.next` or `RingLogger.attach`. Toolshed
-  helpers are available. Type `h Toolshed` for details.
-
-  If connecting via ssh, type `exit` or `<enter>~.` to disconnect.
-  """)
-end
