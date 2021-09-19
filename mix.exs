70e1569014b694731e79d6e0050a33bc4cfb0c1a
Frank Hunleth <fhunleth@troodon-software.com>
2021-09-18 21:17:19 -0400

    Add input_event to the built-in deps

--- mix.exs
+++ mix.exs
@@ -56,6 +56,7 @@ defmodule NervesLivebook.MixProject do
       {:ramoops_logger, "~> 0.1", targets: @all_targets},
       {:bmp280, "~> 0.2", targets: @all_targets},
       {:scroll_hat, "~> 0.1", targets: @rpi_targets},
+      {:input_event, "~> 0.4", targets: @all_targets},
 
       # Dependencies for specific targets
       {:nerves_system_rpi, "~> 1.15", runtime: false, targets: :rpi},
