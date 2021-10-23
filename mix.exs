51b2ec010b69f25d93d00429829d17367dfadb01
Frank Hunleth <fhunleth@troodon-software.com>
2021-10-23 11:16:05 -0400

    Bump nerves_key dependencies

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule NervesLivebook.MixProject do
       {:circuits_gpio, "~> 1.0 or ~> 0.4", targets: @all_targets},
       {:circuits_i2c, "~> 1.0 or ~> 0.3", targets: @all_targets},
       {:circuits_spi, "~> 1.0 or ~> 0.1", targets: @all_targets},
-      {:nerves_key, "~> 0.5.5", targets: @all_targets},
+      {:nerves_key, "~> 1.0", targets: @all_targets},
       {:pigpiox, "~>0.1", targets: @rpi_targets},
       {:ramoops_logger, "~> 0.1", targets: @all_targets},
       {:bmp280, "~> 0.2", targets: @all_targets},
