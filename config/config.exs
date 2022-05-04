14493ef9d7e2fbf6a45e4abcbda9194f4a867644
Jon Carstens <jjcarstens@me.com>
2022-05-03 19:53:20 -0600

    Add Nerves welcome card to Explore section
    Fixes #101

--- config/config.exs
+++ config/config.exs
@@ -29,7 +29,27 @@ config :mime, :types, %{
 config :livebook, :storage, Livebook.Storage.Ets
 
 # Livebook's explore section is built at compile-time
-config :livebook, :explore_notebooks, []
+config :livebook, :explore_notebooks, [
+  %{
+    # Required notebook path
+    path: "#{File.cwd!()}/priv/welcome.livemd",
+    # Optional notebook identifier for URLs, as in /explore/notebooks/{slug}
+    # By default the slug is inferred from file name, so there is no need to set it
+    slug: "nerves",
+    # Optional list of images
+    # image_paths: [
+    #  # This image can be sourced as images/myimage.jpg in the notebook
+    #  "/path/to/myimage.jpg"
+    # ],
+    # Optional details for the notebook card. If omitted, the notebook
+    # is hidden in the UI, but still accessible under /explore/notebooks/{slug}
+    details: %{
+      cover_path: "#{File.cwd!()}/assets/nerves.png",
+      description:
+        "Get to know how Livebook works with Nerves and try out Nerves-specific examples."
+    }
+  }
+]
 
 if Mix.target() == :host do
   import_config "host.exs"
