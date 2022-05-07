03e87ec76bbc2ce9054c7c321ab390eb8673063e
Frank Hunleth <fhunleth@troodon-software.com>
2022-05-07 08:48:02 -0600

    Return built-in packages in the dependency search
    Fixes #192

--- lib/nerves_livebook/dependencies.ex
+++ lib/nerves_livebook/dependencies.ex
@@ -1,22 +1,57 @@
 defmodule NervesLivebook.Dependencies do
   @moduledoc false
 
+  # List of apps that are provided by default by Erlang and Elixir
+  @extra_applications [
+    :asn1,
+    :compiler,
+    :crypto,
+    :debugger,
+    :eex,
+    :elixir,
+    :ex_unit,
+    :ftp,
+    :hex,
+    :inets,
+    :iex,
+    :kernel,
+    :logger,
+    :mnesia,
+    :mix,
+    :nerves_livebook,
+    :observer,
+    :odbc,
+    :os_mon,
+    :parsetools,
+    :public_key,
+    :runtime_tools,
+    :sasl,
+    :snmp,
+    :ssh,
+    :ssl,
+    :stdlib,
+    :syntax_tools,
+    :tftp,
+    :tools,
+    :wx,
+    :xmerl
+  ]
+
   @doc """
   Return included packages that can be used in Nerves Livebook
-
-  TODO: Compute this dynamically
   """
   @spec packages() :: [Livebook.Runtime.package()]
   def packages() do
-    # Return dependencies in this format:
-    [
+    for {app, description, version} <- Application.loaded_applications(),
+        app not in @extra_applications do
       %{
-        dependency: {:kino, "~> 0.6.1"},
-        description: "Interactive widgets for Livebook",
-        name: "kino",
-        url: "https://hex.pm/packages/kino",
-        version: "0.6.1"
+        dependency: {app, "~> #{version}"},
+        description: to_string(description),
+        name: to_string(app),
+        url: "https://hex.pm/packages/#{app}",
+        version: to_string(version)
       }
-    ]
+    end
+    |> Enum.sort()
   end
 end
---
+++ test/nerves_livebook/dependencies_test.exs
@@ -0,0 +1,68 @@
+defmodule NervesLivebook.DependenciesTest do
+  use ExUnit.Case, async: true
+  alias NervesLivebook.Dependencies
+
+  defp all_package_names() do
+    for %{name: name} <- Dependencies.packages(), do: name
+  end
+
+  test "returns reasonable number of packages" do
+    packages = Dependencies.packages()
+
+    assert Enum.count(packages) > 50
+  end
+
+  test "returns some nerves-related packages" do
+    names = all_package_names()
+    assert "circuits_i2c" in names
+    assert "circuits_spi" in names
+    assert "circuits_uart" in names
+    assert "vintage_net" in names
+    assert "vintage_net_wifi" in names
+  end
+
+  test "packages are alphabetized" do
+    names = all_package_names()
+    resorted = Enum.sort(names)
+
+    assert names == resorted
+  end
+
+  test "does not return OTP libraries" do
+    names = all_package_names()
+    refute "kernel" in names
+    refute "stdlib" in names
+    refute "ssh" in names
+    refute "ssl" in names
+    refute "crypto" in names
+  end
+
+  test "does not return nerves_livebook" do
+    names = all_package_names()
+    refute "nerves_livebook" in names
+  end
+
+  test "version numbers all parse" do
+    packages = Dependencies.packages()
+
+    for %{dependency: {name, requirement}, version: version} <- packages do
+      assert {:ok, _} = Version.parse(version), "expected #{version} to parse for #{name}"
+
+      assert {:ok, _} = Version.parse_requirement(requirement),
+             "expected #{requirement} to parse for #{name}"
+
+      assert Version.match?(version, requirement),
+             "expected #{version} to work with #{requirement} for #{name}"
+    end
+  end
+
+  @tag slow: true
+  test "urls all work" do
+    packages = Dependencies.packages()
+
+    for %{name: name, url: url} <- packages do
+      response = Req.get!(url)
+      assert response.status == 200, "expected #{url} to work for #{name}"
+    end
+  end
+end
