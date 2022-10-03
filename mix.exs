13d97ee10a61f95463082856b2ee626b6480b63a
Frank Hunleth <fhunleth@troodon-software.com>
2022-10-03 18:47:19 -0400

    Bump Nerves systems to nerves_system_br 1.20.6 versions

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule NervesLivebook.MixProject do
   defp deps do
     [
       # Dependencies for host and target
-      {:nerves, "~> 1.8.0", runtime: false},
+      {:nerves, "~> 1.9.1", runtime: false},
       {:shoehorn, "~> 0.9.0"},
       {:ring_logger, "~> 0.8.1"},
       {:toolshed, "~> 0.2.13"},
