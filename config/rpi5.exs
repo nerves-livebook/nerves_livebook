58efc6e0caf5f9c38473edf62720e5ed76da9526
Frank Hunleth <fhunleth@troodon-software.com>
2026-06-07 16:18:52 -0400

    Don't try USB gadget on the RPi5
    This only works on the CM5 and if you know how to make it work, adding
    this back won't be that hard.

--- config/rpi5.exs
+++ config/rpi5.exs
@@ -4,7 +4,6 @@ import Config
 # See https://github.com/nerves-networking/vintage_net for more information
 config :vintage_net,
   config: [
-    {"usb0", %{type: VintageNetDirect}},
     {"eth0", %{type: VintageNetEthernet, ipv4: %{method: :dhcp}}},
     {"wlan0", %{type: VintageNetWiFi}}
   ]
